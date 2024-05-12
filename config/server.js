82a28079db0600597cd0c8317f1e5c49ed447665
mateuszkwasniak <mz.kwasniak@gmail.com>
2024-05-12 21:03:39 +0200

    update server.js file

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("", "https://bizzare-jelly-strapi-production.up.railway.app"),
   proxy: true,
   app: {
     keys: env.array("APP_KEYS"),
